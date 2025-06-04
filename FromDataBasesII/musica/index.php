cfca18fca93251cf6e17054c3cdb5fdfe73a2aae
Adro-Ornelas <a22300918@ceti.mx>
2025-06-03 22:36:38 -0600

    Interfaz de navegación añadida

---
+++ FromDataBasesII/musica/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="./CSS/navigate.css">
+</head>
+<body>
+    <h1>Panel de Navegación</h1>
+    <div class="btn-group-custom">
+        <a href="cancion/index.php" class="btn">Canciones</a>
+        <a href="playlist/index.php" class="btn">Playlists</a>
+        <a href="usuario/index.php" class="btn">Usuarios</a>
+    </div>
+</body>
+</html>
