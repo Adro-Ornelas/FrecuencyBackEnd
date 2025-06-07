2746a714970b41b3b5066c1630dcd8467eec13ce
Adro-Ornelas <a22300918@ceti.mx>
2025-06-07 10:54:26 -0600

    Order of main page's options changed

--- FromDataBasesII/musica/index.php
+++ FromDataBasesII/musica/index.php
@@ -8,13 +8,13 @@
 <body>
     <h1>Panel de Navegación</h1>
     <div class="btn-group-custom">
+        <a href="discografia/index.php" class="btn">Discografías</a>
+        <a href="album/index.php" class="btn">Álbum</a>
+        <a href="artista/index.php" class="btn">Artistas</a>
+        <a href="genero/index.php" class="btn">Géneros</a>
         <a href="cancion/index.php" class="btn">Canciones</a>
         <a href="playlist/index.php" class="btn">Playlists</a>
         <a href="usuario/index.php" class="btn">Usuarios</a>
-        <a href="artista/index.php" class="btn">Artistas</a>
-        <a href="discografia/index.php" class="btn">Discografías</a>
-        <a href="genero/index.php" class="btn">Géneros</a>
-        <a href="album/index.php" class="btn">Álbum</a>
         <a href="escucha/index.php" class="btn">Escucha</a>
     </div>
 </body>
